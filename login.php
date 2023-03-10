713c50f3cf2abcc30c4e69142f1a51aeb0f669fe
mogalaxy <mogalaxy2020131@gmail.com>
2023-03-10 19:08:52 +0300

    Update login.php

--- login.php
+++ login.php
@@ -53,35 +53,47 @@ if(isset($message)){
 <!------ Include the above in your HEAD tag ---------->
 
 <body align="center">
-   <br><br><br>
-    <div id="login">
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info">Login</h3>
-                            <div class="form-group">
-                                <label for="email" class="text-info">Email:</label><br>
-                                <input type="email" name="email" id="email" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br>
-                                <input type="text" name="password" id="password" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
-                            </div>
-                            <div id="register-link" class="text-right">
-                                <a href="register.php" class="text-info">Register here</a>
-                            </div>
-                        </form>
+   <div id="template-bg-1">
+    <div
+        class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+        <div class="card p-4 text-light bg-dark mb-5">
+            <div class="card-header">
+                <h3>Sign In</h3>
+            </div>
+            <div class="card-body w-100">
+                <form name="login" action="" method="post">
+                    <div class="input-group form-group mt-3">
+                        <div class="bg-secondary rounded-start">
+                            <span class="m-3"><i
+                                class="fas fa-user mt-2"></i></span>
+                        </div>
+                        <input type="email" class="form-control"
+                            placeholder="email" name="email">
                     </div>
+                    <div class="input-group form-group mt-3">
+                        <div class="bg-secondary rounded-start">
+                            <span class="m-3"><i class="fas fa-key mt-2"></i></span>
+                        </div>
+                        <input type="password" class="form-control"
+                            placeholder="password" name="password">
+                    </div>
+
+                    <div class="form-group mt-3">
+                        <input type="submit" value="Login"
+                            class="btn bg-secondary float-end text-white w-100"
+                            name="login-btn">
+                    </div>
+                </form>
+			</div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-center">
+                    <div class="text-primary">If you are a registered
+                        user, login here.</div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </body>
 </body>
 </html>
